47c0a34da6b6bbb224fa81690327f3ac044b173b
juanfelipe <felipe.rivero@hotmail.com>
2025-05-06 23:09:17 -0500

    excercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -36,6 +36,16 @@ app.get('/info', (request, response) => {
     response.send(info + dateInfo)
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).send('<h1>Person not found</h1>')
+    }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
