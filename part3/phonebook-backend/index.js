89c55e6120e09bfdb0fb607fd494a38952be1955
juanfelipe <felipe.rivero@hotmail.com>
2025-05-06 23:05:26 -0500

    excercises 3.1 - 3.2

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+});
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    const info = `<p>Phonebook has info for ${persons.length} people</p>`
+    const dateInfo = `<p>${date}</p>`
+    console.log(info + dateInfo)
+    response.send(info + dateInfo)
+});
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
