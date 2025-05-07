ec7a9c19885a0dc07a1dde905038360d78135c20
juanfelipe <felipe.rivero@hotmail.com>
2025-05-06 23:29:11 -0500

    excercise 3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 
-persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -52,6 +53,32 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 1000000);
+    return id
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+
+    if (!body.name) {
+        return response.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
