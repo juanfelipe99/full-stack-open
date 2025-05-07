47212b18f1afae1f59a13f76ade5a56856ba5898
juanfelipe <felipe.rivero@hotmail.com>
2025-05-06 23:34:43 -0500

    excercise 3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -64,17 +64,24 @@ app.post('/api/persons', (request, response) => {
     const body = request.body
     console.log(body)
 
-    if (!body.name) {
+    if (!body.name || !body.number) {
         return response.status(400).json({ 
-            error: 'content missing' 
+            error: 'name or number missing' 
         })
-    }
+    };
+
+    const existingPerson = persons.find(person => person.name === body.name)
+    if (existingPerson) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    };
 
     const person = {
         name: body.name,
         number: body.number,
         id: generateId(),
-    }
+    };
 
     persons = persons.concat(person)
 
