887507a68b1017ecdbe6023cfbd272913fdd8eda
juanfelipe <felipe.rivero@hotmail.com>
2025-05-20 22:16:38 -0500

    excercise 3.19 v2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -10,6 +10,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -91,7 +93,7 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -105,9 +107,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
