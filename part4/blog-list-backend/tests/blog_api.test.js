33a1bb93227eb13f6aefca3d2aa836552393ca94
juanfelipe <felipe.rivero@hotmail.com>
2025-06-18 23:25:53 -0500

    excercise 4.8

---
+++ part4/blog-list-backend/tests/blog_api.test.js
@@ -0,0 +1,40 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'book1',
+    author: 'author1',
+    url: 'url1'
+  },
+  {
+    title: 'book2',
+    author: 'author2',
+    url: 'url2'
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let noteObject = new Blog(initialBlogs[0])
+  await noteObject.save()
+  noteObject = new Blog(initialBlogs[1])
+  await noteObject.save()
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
