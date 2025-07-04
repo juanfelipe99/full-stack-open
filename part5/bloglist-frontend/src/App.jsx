b7f0a6c893b2ce8ab507a9348a62b1b261e7a108
juanfelipe <felipe.rivero@hotmail.com>
2025-07-04 15:27:30 -0500

    excercise 5.1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <p>Use the form below to log in.</p>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  const blogForm = () => (
+    <div>
+      <div>
+        {blogs.map(blog => 
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null ?
+      loginForm() :
+      <div>
+        <h2>Blogs</h2>
+        <p>{user.name} logged in</p>
+        {blogForm()}
+      </div>
+    }
+    </div>
+  )
+}
+
+export default App
