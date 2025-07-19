a0602b571af38345b1d0f8cca0b5fc2f9d9e5a7d
juanfelipe <felipe.rivero@hotmail.com>
2025-07-19 14:47:18 -0500

    excercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+    <div style={{ border: '2px solid black', borderRadius: '6px', padding: '16px' }}>
+      <div>{blog.title}</div>
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'Hide details' : 'Show details'}
+      </button>
+      {showDetails && (
+        <div>
+          <div>Author: {blog.author}</div>
+          <div>URL: {blog.url}</div>
+          {blog.likes !== undefined && <div>Likes: {blog.likes}</div>}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
